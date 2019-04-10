1c752e3dad86acd2084eb7a167c1d706eac4600f
neg0diay <neg@front-tech.ru>
2019-04-10 08:01:45 +0300

    launch arguments

--- pointsaggregator.py
+++ pointsaggregator.py
@@ -2,17 +2,46 @@ from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql.types import DoubleType
 from pyspark.sql.functions import sum as _sum, desc
+import argparse
 
 # conf = SparkConf().setAppName('PointsAggregatorStandaloneApp')
 # sc = SparkContext(conf=conf)
+
+environment = ""
+files_directory = ""
 
 spark = SparkSession \
     .builder \
     .appName("PointsAggregatorStandaloneApp").getOrCreate()
 
 
+def parseInputArguments():
+    global files_directory, environment
+    parser = argparse.ArgumentParser(description='group points')
+    parser.add_argument('--directory', dest='directory',
+                        default="./data/*.orc",
+                        help='full path to orc files')
+    parser.add_argument('--environment', dest='environment',
+                        default="local",
+                        help='name of environment(local or cluster)')
+    args = parser.parse_args()
+    files_directory = args.directory
+    environment = args.environment
+    print("Current file directory: {}".format(files_directory))
+    print("Current environment: {}".format(environment))
+
+
 def getGroupedPointOwners():
-    points_df = spark.read.format("orc").load("./data/*.orc")
+    if environment == "local":
+        pass
+    elif environment == "cluster":
+        pass
+    else:
+        raise AssertionError(
+            "Bad environment variable (environment = {}). Should be local or cluster".format(environment))
+
+    points_df = spark.read.format("orc").load(files_directory)
+
     points_df = points_df.withColumn("qty", points_df["qty"].cast(DoubleType()))
     print(points_df.printSchema)
     points_df.show(10)
@@ -25,4 +54,6 @@ def getGroupedPointOwners():
 
 
 if __name__ == '__main__':
+    # pass
+    parseInputArguments()
     points_stats = getGroupedPointOwners()
